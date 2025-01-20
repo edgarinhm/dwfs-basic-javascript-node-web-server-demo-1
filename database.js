bc452356f48ae544aefa538f9909762886dd26fd
Edgar Mejia <edgarinhm@hotmail.com>
2025-01-20 11:08:02 -0500

    add api port env

--- database.js
+++ database.js
@@ -1,8 +1,9 @@
 const { Sequelize } = require('sequelize');
-const { API_NAME, API_KEY, API_SECRET, API_HOST, API_DB } = require('./constants/environment-constants');
+const { API_NAME, API_KEY, API_SECRET, API_HOST, API_DB, API_PORT } = require('./constants/environment-constants');
 
 const sequelize = new Sequelize(API_NAME, API_KEY, API_SECRET, {
     host: API_HOST,
+    port: API_PORT,
     dialect: API_DB,
 });
 
