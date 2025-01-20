d67938e7121ac6abd1600d08d80b4eb856b6de27
Edgar Mejia <edgarinhm@hotmail.com>
2025-01-20 13:48:07 -0500

    add app port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const sequelize = require('./database');
-const { PORT } = require('./constants/environment-constants');
+const { APP_PORT } = require('./constants/environment-constants');
 const { httpStatus } = require('./constants/http-constants');
 const usuariosRoutes = require('./routes/usuarios');
 const productosRoutes = require('./routes/productos');
@@ -34,7 +34,7 @@ app.use((err, req, res, next) => {
 
 // Start the server
 sequelize.sync().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    app.listen(APP_PORT, () => {
+        console.log(`Servidor corriendo en http://localhost:${APP_PORT}`);
     });
 });
